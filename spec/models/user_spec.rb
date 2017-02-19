c7d35e138595d35b7c72a28f35e51b79463e6dde
Frank <fbelanger@ackroo.com>
2017-02-18 21:59:09 -0500

    spec describing the desired behaviour

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { build :user }
+  subject { user }
+
+  describe 'validations' do
+    context 'when admin = true' do
+      before(:each) { user.admin = true }
+      it { is_expected.to validate_uniqueness_of(:admin)  }
+    end
+  end
 end
